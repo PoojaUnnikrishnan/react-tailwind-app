b126f90973bae82f13dad65345782d38a4d760f2
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-08-11 20:24:54 +0530

    step 1 added

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,21 +1,16 @@
 import React from "react";
-import Button from "./Button";
-import Logo from "./Logo";
-import ProgressBar from "./ProgressBar";
 
 const Step1 = () => {
   return (
-    <div className=" h-screen flex justify-center items-center border-8">
-      <div className=" flex flex-col p-10">
-        <Logo />
-        <ProgressBar />
-        <div className="font-bold flex justify-center px-5 text-3xl">
-          <h2>Welcome! First things first...</h2>
-        </div>
-        <div className="p-3  flex justify-center text-md text-gray-500 ">
-          <p>You can always change them later</p>
-        </div>
-        <div className=" p-5">
+    <>
+      <div className="font-bold flex justify-center text-3xl">
+        <h2>Welcome! First things first...</h2>
+      </div>
+      <div className="p-3  flex justify-center text-md text-gray-500 ">
+        <p>You can always change them later</p>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex flex-col pt-10 w-96">
           <div className="flex flex-col">
             <label className="text-left py-1 text-sm">Full Name</label>
             <input
@@ -30,13 +25,9 @@ const Step1 = () => {
               placeholder="Steve"
             />
           </div>
-
-          <div className="flex  justify-center ">
-            <Button />
-          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
