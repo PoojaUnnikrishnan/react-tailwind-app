e88f957020735d5734a7a9b2ecb345cc058250d7
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-08-11 20:24:22 +0530

    button added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Button = () => {
+const Button = ({ handleClick, steps, currentStep }) => {
   return (
-    <button className="bg-indigo-500 px-6 py-2 w-full mt-4 rounded-md text-white">
-      Create Workspace
+    <button
+      onClick={() => handleClick()}
+      className="bg-indigo-500 px-6 py-2 mt-5 rounded-md text-white hover:bg-indigo-300 w-96"
+    >
+      {currentStep === steps.length ? "Launch Eden" : "Create Workspace"}
     </button>
   );
 };
